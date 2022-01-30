155e128592bb16fd73419f180d0c13eaef3457b5
Blake <blake.elink@gmail.com>
2022-01-30 12:39:57 -0500

    Add files via upload

--- analysis.R
+++ analysis.R
@@ -18,70 +18,81 @@ colnames(dat) <- cnames
 rownames(dat) <- temp
 # swap co2 and h2o columns
 dat <- dat[, c("Ar", "CH4", "CO", "H2O", "H2", "CO2", "He")]
-
-# plot diffusion constant versus temp for each species
-ar <- dat[, "Ar"]
-ch4 <- dat[, "CH4"]
-co <- dat[, "CO"]
-h2o <- dat[, "H2O"]
-h2 <- dat[, "H2"]
-
-plot(
-  temp,
-  ar,
-  type = "b",
-  ylab = "diffusivity (cm^2/s)",
-  xlab = "temp (deg C)",
-  ylim = c(0, 3),
-  xlim = c(20, 400)
-)
-points(temp, ch4, type = "b", col = "green")
-points(temp, co, type = "b", col = "blue")
-points(temp, h2o, type = "b", col = "magenta")
-points(temp, h2, type = "b", col = "red")
-# Add a legend
-legend(
-  20,
-  2.5,
-  legend = c("Ar", "CH4", "CO", "H2O", "H2"),
-  col = c("black", "green", "blue", "magenta", "red"),
-  lty = 1:2,
-  cex = 0.8
-)
-dev.copy(jpeg, 'diffusivity_vs_T_training_data.jpg')
-dev.off()
+# swap cnames co2 and h2o
+cnames = cnames[c(1,2,3,6,5,4,7)]
 
 # add guessed features to dataframe
-# molecular mass, kinetic diameter, polarizability, dipole moment
+# molecular mass, kinetic diameter, dipole moment
 
 # regression model yhat = t(x) %*% beta + nu
 # x is feature vector
 
-fdat <- matrix(ncol = 4, nrow = 7)
+fdat <- matrix(ncol = 3, nrow = 7)
 fdat <- data.frame(fdat, row.names = cnames)
-colnames(fdat) <- c("MM", "KDiam", "Polar.", "Dipole")
+colnames(fdat) <- c("MM", "KDiam", "Dipole")
 # add molar mass (g/mol)
-fdat["MM"] <- c(39.95, 16.04, 28.01, 44.01, 2.02, 18.02, 4.00)
+fdat["MM"] <- c(39.95, 16.04, 28.01, 18.02, 2.02, 44.02, 4.00)
 # add kinetic diameter (picometer) (https://en.wikipedia.org/wiki/Kinetic_diameter)
-fdat["KDiam"] <- c(340, 380, 376, 330, 289, 265, 260)
+fdat["KDiam"] <- c(340, 380, 376, 265, 289, 330, 260)
 # add dipole moment (Debye) (https://cccbdb.nist.gov/diplistx.asp)
-fdat["Dipole"] <- c(0, 0, 0.112, 0, 0, 1.855, 0)
-# polarizabilities (Angstrom^3)
-fdat["Polar."] <- c(1.664, 2.448, 1.953, 2.507, 0.787, 1.501, 0.208)
+fdat["Dipole"] <- c(0, 0, 0.112, 1.855, 0, 0, 0)
 
-# perform least squares fit
 
 # check rms of each feature; consider altering to make rms similar size
-
 # regularize rmses
 fdat[,"KDiam"]<- fdat[,"KDiam"]*(1/1000)
-fdat["MM"] <- fdat["MM"]^(-1/2)
+fdat["MM"] <- 1/sqrt(fdat["MM"])
 
 rms <- function(x) sqrt(sum(x*x)/length(x))
 # print out rms of features
-for(i in 1:4) { 
+for(i in 1:3) { 
   res <- rms(fdat[,i])
   print(paste0(colnames(fdat)[i]," = ", res))
 }
 
-ls1 <- lsfit(fdat, t(dat), wt = NULL, intercept = TRUE, tolerance = 1e-07)
+ls1 <- lsfit(fdat[1:5,], t(dat[,1:5]), wt = NULL, intercept = TRUE, tolerance = 1e-07)
+
+# check model result on co2 and He
+fco2 <- as.numeric(cbind(1,fdat["CO2",]))
+fhe <- as.numeric(cbind(1,fdat["He",]))
+
+model1 <- t(ls1$coefficients)
+rownames(model1) <- temp
+
+pred_co2 <- model1 %*% fco2
+pred_he <- model1 %*% fhe
+actual_co2 <- dat[,"CO2"]
+actual_he <- dat[,"He"]
+
+library("pracma")
+errCo2 <- rmserr(pred_co2,actual_co2)
+errH2 <- rmserr(pred_he,actual_he)
+
+# Plot actual versus predicted
+
+# plot diffusion constant versus temp for the two species we'll predict, co2 and He
+
+plot(
+  temp,
+  dat[, "CO2"],
+  type = "b",
+  ylab = "diffusivity (cm^2/s)",
+  xlab = "temp (deg C)",
+  ylim = c(0, 3),
+  xlim = c(20, 400),
+  col = "blue"
+)
+points(temp,pred_co2,type="b",col="dodgerblue", lty="dashed")
+points(temp, dat[, "He"], type = "b", col = "red")
+points(temp,pred_he,type="b", col="firebrick", lty = "dashed")
+
+legend(
+  20,
+  2.5,
+  legend = c("actual CO2", "pred CO2", "actual He", "pred He"),
+  col = c("blue", "dodgerblue", "red", "firebrick"),
+  lty = 1:2,
+  cex = 0.8
+)
+dev.copy(jpeg, 'diffusivity_as_fxn_of_temp.jpg')
+dev.off()
