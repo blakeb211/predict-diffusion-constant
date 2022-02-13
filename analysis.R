4c3fe6ac93b11d4f86d860e96fbd5137cf5ba962
Blake <blake.elink@gmail.com>
2022-02-13 16:07:57 -0500

    Add files via upload

--- analysis.R
+++ analysis.R
@@ -1,57 +1,63 @@
 # get dataframe into desired form
 # (https://www.engineeringtoolbox.com/air-diffusion-coefficient-gas-mixture-temperature-d_2010.html)
-dat <- read.table("diffusion.dat")
-dat <- t(dat)
+library(magrittr)
+
+dat <- t(read.table("diffusion.dat"))
 cnames <-
-  dat[1,] # save molecular formula to be used as column names
+  dat["Species",] # save chemical formulas to be used as column names
 dat <- matrix(as.double(dat), ncol = 8)
 
 # clean data
 dat <- dat[,-8]  # remove SF6
 cnames <- cnames[-8] # remove SF6 from cnames
-dat <- dat[-1,]   # remove first row
-dat <- dat[-1,]  # remove 0 degree data since some missing
+dat <- dat[-1,]   # remove first row (chemical names)
+dat <- dat[-1,]  # remove first row (0 deg C data)
+
+# swap co2 and h2o columns in dat and cnames
+dat <- dat[, c(1, 2, 3, 6, 5, 4, 7)]
+cnames = cnames[c(1, 2, 3, 6, 5, 4, 7)]
 
 # label dataframe
 temp <-
   c(20, 100, 200, 300, 400) # create vector to hold temp values
 colnames(dat) <- cnames
 rownames(dat) <- temp
-# swap co2 and h2o columns
-dat <- dat[, c("Ar", "CH4", "CO", "H2O", "H2", "CO2", "He")]
-# swap cnames co2 and h2o
-cnames = cnames[c(1, 2, 3, 6, 5, 4, 7)]
 
-# add guessed features to dataframe
-# molecular mass, kinetic diameter, dipole moment
+# Create Feature Matrix
+# Features: MM (molar mass), Vol (molecular volume), Dipole (dipole moment)
 
-# regression model yhat = t(x) %*% beta + nu
-# x is feature vector
-
-# fdat is the feature vector for the 7 molecules
+# fdat is the feature matrix for the 7 molecules
 fdat <- matrix(ncol = 3, nrow = 7)
 fdat <- data.frame(fdat, row.names = cnames)
-colnames(fdat) <- c("MM", "KDiam", "Dipole")
+colnames(fdat) <- c("MM", "Vol", "Dipole")
+
 # add molar mass (g/mol)
 fdat["MM"] <- c(39.95, 16.04, 28.01, 18.02, 2.02, 44.02, 4.00)
-# add kinetic diameter (picometer) (https://en.wikipedia.org/wiki/Kinetic_diameter)
-fdat["KDiam"] <- c(340, 380, 376, 265, 289, 330, 260)
+
+# van der waals b coefficients (L/mol)
+# (https://chem.libretexts.org/Ancillary_Materials/Reference/Reference_Tables/Atomic_and_Molecular_Properties/A8%3A_van_der_Waal%27s_Constants_for_Real_Gases)
+fdat["Vol"] <- c(0.03201, 0.04301, 0.03948, 0.03049, 0.02651, 0.04286, 0.0238)
+
 # add dipole moment (Debye) (https://cccbdb.nist.gov/diplistx.asp)
 fdat["Dipole"] <- c(0, 0, 0.112, 1.855, 0, 0, 0)
 
+# transform MM feature to be 1 / sqrt(MM)
+fdat["MM"] <- 1 / sqrt(fdat["MM"])
+fdat["Vol"] <- 1 / fdat["Vol"]^(1/3)
 
 # check rms of each feature; consider altering to make rms similar size
 # regularize rmses
-fdat[, "KDiam"] <- fdat[, "KDiam"] * (1 / 1000)
-fdat["MM"] <- 1 / sqrt(fdat["MM"])
-
 rms <- function(x)
   sqrt(sum(x * x) / length(x))
-# print out rms of features
-# for (i in 1:3) {
-#   res <- rms(fdat[, i])
-#   print(paste0(colnames(fdat)[i], " = ", res))
-# }
+
+# print out rms of each feature
+ for (i in 1:3) {
+   res <- rms(fdat[, i])
+   print(paste0(colnames(fdat)[i], " = ", res))
+ }
+
+# regression model yhat = t(x) %*% beta + nu
+# x is feature vector, beta is coefficients
 
 ls1 <-
   lsfit(
@@ -114,13 +120,13 @@ ggplot(data=plot_data) + aes(x=Temp) +
   
   cat("\nThe (+/-) signs of the model coefficients show that diffusion constants\n
         are positively correlated with 1/sqrt(Molar Mass) and negatively\n
-        correlated with Kinetic Diameter and Dipole Moment. This makes sense\n
+        correlated with Molecular Volume and Dipole Moment. This makes sense\n
         physically because larger molecules should bump into other molecules\n
         more and molecules with a dipole moment should interact more strongly with\n
         molecules around them which would slow them down.\n")
   
   # calculate diffusion limit of gases in air at these temperatures
-  # diffusion limit is obtained from plugging Molar Mass = 1, Kinetic Diameter = 0, 
+  # diffusion limit is obtained from plugging Molar Mass = 1, Volume = 0, 
   # and Dipole Moment = 1 into the model.
   
   predDiffusionLimit = model1 %*% c(1, 1/sqrt(1), 0, 0)
@@ -128,7 +134,5 @@ ggplot(data=plot_data) + aes(x=Temp) +
   cat("\nThe predicted diffusion limit for a gas at each temperature:\n")
   colnames(predDiffusionLimit) <- "cm^2 / 2"
   print(predDiffusionLimit)
-  
-  
   
   ggsave(filename="model_predictions.jpg", height=4.3, width=4,units="in", dpi=200)
